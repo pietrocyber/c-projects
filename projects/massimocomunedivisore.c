9a8677b35036e72ba49be33bfc4603703fcfd8d5
P3terud <iannarone2006@gmail.com>
2026-02-13 15:09:04 +0100

    Create massimocomunedivisore.c

---
+++ projects/massimocomunedivisore.c
@@ -0,0 +1,11 @@
+/*stiamo calcolando il massimo comune divisore, immettendo due numeri razionali*/
+
+
+#include <stdio.h>
+
+int main(){
+    int n1, n2, d1, d2;
+    printf("Inserire i numeratore dei due numeri razionali!");
+    scanf("%d" "%d", &n1 , &n2);
+    return 0;
+}
