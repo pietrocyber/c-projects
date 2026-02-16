4088c152a3c2b47583cb6f9aee10726662f17dec
P3terud <iannarone2006@gmail.com>
2026-02-16 12:33:12 +0100

    Create minuti.c

---
+++ minuti.c
@@ -0,0 +1,10 @@
+/*Immettendo un valore in minuti, convertire il numero in ore e minuti*/
+#include <stdio.h>
+int main(){
+    int numero, ore, minuti;
+    printf("Inserisci il numero di minuti:");
+    scanf("%d",&numero);
+    ore = numero/60;
+    minuti = (numero % 60);
+    printf("%d h, %d m", ore, minuti);
+}
