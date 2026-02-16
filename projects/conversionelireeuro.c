718490af1cb05fc44f1217ca6b97f7b85131d00d
P3terud <iannarone2006@gmail.com>
2026-02-16 22:37:20 +0100

    Create conversionelireeuro.c

---
+++ projects/conversionelireeuro.c
@@ -0,0 +1,13 @@
+/*Immetendo il valore delle lire che abbiamo, dobbiamo convertirle in euro*/
+#include <stdio.h>
+#include <math.h>
+int main(){
+    double lire, conversione, arrotondamento;
+    const double tassoconversione= 1936.27;
+    printf("Inserire l'importo di lire da convertire:");
+    scanf("%lf", &lire);
+    conversione=lire/tassoconversione;
+    arrotondamento=ceil(conversione*100)/100;
+    printf("L'importo in euro è: %lf", arrotondamento);
+    return 0;
+}
