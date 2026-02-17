11e8ce423649ac2003567712fe6944f0ec98a61d
P3terud <iannarone2006@gmail.com>
2026-02-17 11:29:16 +0100

    Update areatriangolo.c

--- projects/areatriangolo.c
+++ projects/areatriangolo.c
@@ -1,10 +1,9 @@
 /*Calcoliamo l'area di un triangolo, con base e altezza forniti in input*/
 #include <stdio.h>
 int main(){
-    int base, altezza;
-    float area;
+    int base, altezza, area;
     printf("Inserire la base e l'altezza in cm:");
-    scanf("%d %d", &base, &altezza);
+    scanf("%f %f", &base, &altezza);
     area= (base*altezza)/2;
     printf("L'area del triangolo: %f cm^2", area);
     return 0;
