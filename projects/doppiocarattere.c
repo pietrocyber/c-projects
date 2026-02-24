3a35d50daf02334edeb3b3b4cfda0fa98db00dc0
P3terud <iannarone2006@gmail.com>
2026-02-24 12:54:47 +0100

    Delete doppiocarattere.c

--- projects/doppiocarattere.c
+++
@@ -1,9 +0,0 @@
-/*Immettendo due caratteri stampare in doppio i due caratteri, nell'ordine inverso*/
-#include <stdio.h>
-int main(){
-    char a, b;
-    printf("Inserire i due caratteri: ");
-    scanf("%c%c", &a, &b);
-    printf("%c%c%c%c", b,a,b,a);
-    return 0;
-}
