474c3871808f0ddf477d7d1b34d7d2d679c40c3d
P3terud <iannarone2006@gmail.com>
2026-02-24 12:54:36 +0100

    Delete carattereprecedente.c

--- projects/carattereprecedente.c
+++
@@ -1,9 +0,0 @@
-/*Immettendo un carattere, stampare i due caratteri precedenti*/
-#include <stdio.h>
-int main(){
-    char a;
-    printf("Inserire il carattere:");
-    scanf("%c", &a);
-    printf("%c%c", a-1, a-2);
-    return 0;
-}
