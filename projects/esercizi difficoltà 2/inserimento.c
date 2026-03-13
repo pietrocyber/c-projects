94a9d3062e1380db92c7df024169861d08acac5c
P3terud <iannarone2006@gmail.com>
2026-03-13 15:58:49 +0100

    Update inserimento.c

--- projects/esercizi difficoltà 2/inserimento.c
+++ projects/esercizi difficoltà 2/inserimento.c
@@ -9,7 +9,7 @@ int main(){
     printf("Quanti interi vuoi inserire? ");
     scanf("%d", &n);
     
-    a=malloc((n+1)*sizeof(int));
+    a=(int*)malloc((n+1)*sizeof(int));
 
     printf("Immettere l'array di interi: ");
     for(i=0;i<n;i++){
