f9f4dbef3007c7db7dab5938fbcbc979e778c3f3
P3terud <iannarone2006@gmail.com>
2026-03-13 21:10:06 +0100

    Update arrayordinato.c

--- projects/esercizi difficoltà 2/arrayordinato.c
+++ projects/esercizi difficoltà 2/arrayordinato.c
@@ -3,15 +3,26 @@
 #include <stdlib.h>
 
 int main(){
-    int *a,n;
+    int *a,rp, num, i, j;
     
     printf("Quanti numeri vuoi inserire? ");
-    scanf("%d", &n);
+    scanf("%d", &rp);
 
-    a=(int*)malloc(n*sizeof(int));
-    for(i=1, i<n; i++){
+    a=(int*)malloc(rp*sizeof(int));
+    
+    printf("Inserisci i numeri: ");
+    for(i=0; i<rp; i++){
+        scanf("%d", &num);
         
+        for(j=i-1; (j>=0 && a[j]>num);j--){
+            a[j+1]=a[j];
+        }
+        a[j+1]=num;
+    }
+    for(i=0;i<rp;i++){
+        printf("%d ", a[i]);
     }
     free(a);
+    
     return 0;
 }
