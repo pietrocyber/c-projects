dddf88c529752d49f241799d14d6b6ec2c9651f3
P3terud <iannarone2006@gmail.com>
2026-03-15 17:53:46 +0100

    Create contarecarattere.c

---
+++ projects/esercizi difficoltà 2/contarecarattere.c
@@ -0,0 +1,26 @@
+/*Contare quante volte, il carattere scelto esiste nella stringa*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char *s, c;
+    int i, cn=0;
+
+    s=(char*)malloc(1000*sizeof(char));
+
+    printf("Inserisci una stringa: ");
+    fgets(s, 1000, stdin);
+
+    printf("Inserire il carattere da cercare: ");
+    scanf(" %c", &c);
+    
+    for(i=0; s[i]!= '\0'; i++){
+        if(c==s[i]) cn++;
+    }
+    
+    printf( "Il carattere, c'è nella frase %d volte", cn);
+
+    free(s);
+    return 0;
+}
