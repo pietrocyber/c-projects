293f14a0b2437a01a8665c1f443d52c9385776f0
P3terud <iannarone2006@gmail.com>
2026-03-17 21:25:37 +0100

    Update eliminazionecaratterecentrale.c

--- projects/esercizi difficoltà 2/eliminazionecaratterecentrale.c
+++ projects/esercizi difficoltà 2/eliminazionecaratterecentrale.c
@@ -3,8 +3,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void shiftsinistra(char *,int);
 int main(){
     const int CD=100;
+    int i, div;
     char a[CD];
+
+    printf("Inserisci la stringa: ");
+    scanf("%s", a);
     
+    i=0;
+    while(a[i]!='\0'){
+        i++;
+    }
+    div=i/2;
+    
+    shiftsinistra(a,div);
+
+    printf("La stringa aggiornata è:\n%s\n", a);
+    return 0;
+}
+void shiftsinistra(char *x, int y){
+    int i;
+    for(i=y;x[i]!='\0';i++){
+        x[i]=x[i+1];
+    }
 }
