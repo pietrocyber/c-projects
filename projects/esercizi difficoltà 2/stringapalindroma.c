b4b851362686d331fc22c2980e3990b990983d23
P3terud <iannarone2006@gmail.com>
2026-03-17 22:02:07 +0100

    Create stringapalindroma.c

---
+++ projects/esercizi difficoltà 2/stringapalindroma.c
@@ -0,0 +1,33 @@
+/*Immettere una stringa e controllare se è palindroma*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    const int CD=100;
+    char s[CD];
+    int i, rp, j, trovato=0;
+
+    printf("Inserisci una stringa: ");
+    scanf("%s", s);
+
+    i=0;
+    while(s[i]!='\0'){
+        i++;
+    }
+    rp=i;
+
+    for(j=0;s[j]!='\0';j++){
+        if(s[j]==s[rp-1]){
+            trovato=1;
+        }
+    }
+    
+    if(trovato==1){
+        printf("La stringa è palindroma!");
+    }else{
+        printf("La stringa non è palindroma!");
+    }
+
+    return 0;
+}
