382c523d17e0de55ed00398d7ebb0c729b13c3f1
P3terud <iannarone2006@gmail.com>
2026-03-18 20:13:32 +0100

    Create ricercacaratteri.c

---
+++ projects/esercizi difficoltà 2/ricercacaratteri.c
@@ -0,0 +1,41 @@
+/*Immettere una stringa e ricercare una serie di caratteri*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int search(char *x, char *y);
+
+int main(){
+    const int CD = 100;
+    char a[CD], b[CD];
+
+    printf("Inserisci una stringa: ");
+    fgets(a, CD, stdin);
+
+    printf("Inserisci i caratteri da cercare: ");
+    scanf("%s", b);
+
+    if(search(a, b)){
+        printf("Trovato!\n");
+    } else {
+        printf("Non trovato!\n");
+    }
+
+    return 0;
+}
+
+int search(char *x, char *y){
+    int i, j;
+
+    for(i = 0;  x[i] != '\n'; i++){
+        if(x[i] == y[0]){
+            for(j = 0; y[j] != '\0' && x[i+j] != '\n' && x[i+j] == y[j]; j++);
+
+            if(y[j] == '\0'){ 
+                return 1;
+            }
+        }
+    }
+
+    return 0; 
+}
