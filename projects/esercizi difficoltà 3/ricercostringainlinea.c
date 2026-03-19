72a7e78f7521425e0da4bf5355c174773f174752
P3terud <iannarone2006@gmail.com>
2026-03-19 12:35:56 +0100

    Create ricercostringainlinea.c

---
+++ projects/esercizi difficoltà 3/ricercostringainlinea.c
@@ -0,0 +1,36 @@
+/*Inserire una linea e una stringa, vedere se la stringa è contenutà nella linea*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int search(char *, char*);
+
+int main(){
+    const int CD=100;
+    char a[CD], b[CD];
+
+    printf("Inserisci la linea: ");
+    fgets(a, CD, stdin);
+
+    printf("Inserisci la stringa: ");
+    fgets(b, CD, stdin);
+
+    if(search(a, b)){
+        printf("Trovato!\n");
+    }else{
+        printf("Non trovato!\n");
+    }
+
+    return 0;
+}
+
+int search(char *x, char *y){
+    int i, j;
+    for(i=0; x[i]!='\n'; i++){
+        for(j=0;x[j+i]!='\n';j++);
+        if(x[i]==x[j+1]){
+            return 1;
+        }
+    }
+    return 0;
+}
