7a0f7d10043e1d6fe2533ea551bd5d73a036b5d8
P3terud <iannarone2006@gmail.com>
2026-03-24 12:54:13 +0100

    Update sommareinteri.c

--- projects/esercizi difficoltà 3/sommareinteri.c
+++ projects/esercizi difficoltà 3/sommareinteri.c
@@ -1,24 +1,16 @@
-/*Sommare gli interi immessi (Sommare gli interi passati su riga di comando)*/
+/*Sommare gli interi immessi su riga di comando*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int rp, *a, somma=0, i;
+int main(int argc, char *argv[]){
+    int somma=0, i;
 
-    printf("Quanti interi vuoi inserire? ");
-    scanf("%d", &rp);
-
-    a=(int*)malloc(rp*sizeof(int));
-
-    printf("Inserisci gli elementi:\n");
-    for(i=0;i<rp;i++){
-        scanf("%d", &a[i]);
-        somma=somma+a[i];
+    for(i=1;i<argc;i++){
+        somma+=atoi(argv[i]);
     }
 
     printf("La somma è : %d", somma);
 
-    free(a);
     return 0;
 }
