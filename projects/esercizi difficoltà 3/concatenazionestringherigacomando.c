a9aa2e02a374e3234ba7a32dd2aef8cbbdb9fec5
P3terud <iannarone2006@gmail.com>
2026-04-19 11:59:47 +0200

    Update concatenazionestringherigacomando.c

--- projects/esercizi difficoltà 3/concatenazionestringherigacomando.c
+++ projects/esercizi difficoltà 3/concatenazionestringherigacomando.c
@@ -2,24 +2,11 @@
 
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
-    const int CD=100;
-    int i,c=0, j;
-    char a[CD];
-
+int main(int argc,char*argv[]){
+    int i;
+    printf("La frase concatenata è: ");
     for(i=1;i<argc;i++){
-       for(j=0;argv[i][j]!='\0';j++){
-            a[c++]=argv[i][j];
-        }
-            
-        if(i<argc-1){
-            a[c++]=' ';
-        }
+        printf("%s ",argv[i]);
     }
-    a[c]='\0';
-    
-    
-    printf("La frase concatenata è: %s\n", a);
-    
     return 0;
 }
